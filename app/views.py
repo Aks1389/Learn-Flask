1d7c80c809e3f08ee436a25a84182db5eb65b01b
Aks1389 <aksnv511@gmail.com>
2020-09-14 15:54:31 +0300

    Ls13. Uploading files

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, jsonify, make_response
 from datetime import datetime
+import os
+from werkzeug.utils import secure_filename
 
 @app.template_filter("clean_date")
 def clean_date(dt):
@@ -157,3 +159,49 @@ def query():
         return f"(Query) {serialized}", 200
     else:
         return "No query received", 200
+
+app.config["IMAGE_UPLOADS"] = "C:\\Users\\aksnv\\Documents\\Programming\\Learn-Flask\\app\\static\\img\\upploads"
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG", "JPG", "JPEG", "GIF"]
+app.config["MAX_IMAGE_FILESIZE"] = 0.5 * 1024 * 1024
+
+def allowed_image(filename):
+    if not '.' in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
+
+def allowed_image_filesize(filesize):
+    if int(filesize) <= app.config["MAX_IMAGE_FILESIZE"]:
+        return True
+    else:
+        return False
+
+@app.route("/upload-image", methods=["GET", "POST"])
+def upload_image():
+    if request.method == "POST":
+        if request.files:
+
+            image = request.files["image"]
+            
+            if image.filename == "":
+                print("Image must have a filename")
+                return redirect(request.url)
+            
+            if not allowed_image(image.filename):
+                print("That image extensions is not allowed")
+                return redirect(request.url)
+            else:
+                filename = secure_filename(image.filename)
+                image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+
+            filesize = request.cookies.get("filesize")
+            if not allowed_image_filesize(filesize):
+                print("File exceeded maximum size!")
+                return redirect(request.url)
+            print("Image saved")
+            return redirect(request.url)
+    return render_template("public/upload_image.html")
